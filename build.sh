3eb93154c0a7ba0883ec3bede33fe24da61c0b31
Falaxir <61189667+Falaxir@users.noreply.github.com>
2024-11-02 21:51:04 +0100

    Update build.sh test gstreamer libraries build

--- build.sh
+++ build.sh
@@ -44,11 +44,14 @@ EOF
 
 dnf install -y netbird
 
+#TEST 64BIT GSTREAMER
+rpm-ostree install gstreamer1 gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-extras gstreamer1-plugins-ugly gstreamer1-libav gstreamer1-tools gstreamer1-gtk gstreamer1-qt gstreamer1-alsa gstreamer1-pulseaudio gstreamer1-gl
+
+#TEST 32BIT GSTREAMER
+rpm-ostree install gstreamer1.i686 gstreamer1-plugins-base.i686 gstreamer1-plugins-good.i686 gstreamer1-plugins-bad-free.i686 gstreamer1-plugins-bad-free-extras.i686 gstreamer1-plugins-ugly.i686 gstreamer1-libav.i686 gstreamer1-tools.i686 gstreamer1-gtk.i686 gstreamer1-qt.i686 gstreamer1-alsa.i686 gstreamer1-pulseaudio.i686 gstreamer1-gl.i686
+
 #Gstreamer 64bits
-rpm-ostree install gstreamer1 gstreamer1-vaapi gstreamer1-plugin-libav gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-bad-free gstreamer1-plugins-ugly-free
+#rpm-ostree install gstreamer1 gstreamer1-vaapi gstreamer1-plugin-libav gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-bad-free gstreamer1-plugins-ugly-free
 
 #Gstreamer 32bits
-rpm-ostree install gstreamer1.i686 gstreamer1-vaapi.i686 gstreamer1-plugins-base.i686 gstreamer1-plugins-good.i686 gstreamer1-plugins-bad-free.i686 gstreamer1-plugins-ugly-free.i686
-
-# Gstreamer plugins recommanded
-#rpm-ostree install gstreamer1-devel gstreamer1-plugins-base-tools gstreamer1-doc gstreamer1-plugins-base-devel gstreamer1-plugins-good gstreamer1-plugins-good-extras gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-devel gstreamer1-plugins-bad-free-extras
+#rpm-ostree install gstreamer1.i686 gstreamer1-vaapi.i686 gstreamer1-plugins-base.i686 gstreamer1-plugins-good.i686 gstreamer1-plugins-bad-free.i686 gstreamer1-plugins-ugly-free.i686
